4d64b92d13664bad70c6b2b80ef5e54d63d742de
Edv-dev <edvslplus@gmail.com>
2022-01-05 12:15:18 -0300

    Add sound
    Adicionado efeito de tiro

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -5,12 +5,21 @@ var speed = 400
 var dirx = 0
 var diry = 0
 const TIRO = preload("res://Scenes/Tiro.tscn")
+onready var tiro = get_node("res://Scenes/Tiro.tscn")
+
+
 func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
 	dirx = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	diry = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	
+	if dirx > 0:
+		$Pos.position.x = 44
+	if dirx < 0:
+		$Pos.position.x -44
+		
 	moviment.x = dirx * speed
 	moviment.y = diry * speed
 	moviment = move_and_slide(moviment, Vector2.UP)
